a71297af51cd1037eba06ad4ce2c1252b971b3d1
Chisadin <tranthanhnam.ccc@gmail.com>
2025-12-16 22:07:11 +0700

    feat: implement HLS download functionality, add playwright and update README

--- patch_local.py
+++ patch_local.py
@@ -1,25 +1,32 @@
 from dotenv import load_dotenv
 from os import getenv
+import pathlib
 
 load_dotenv()
 
-prefix = getenv("MOVIE_ID")
-if not prefix:
-    print("MOVIE_ID is not set.")
-    exit(1)
+# prefix = getenv("MOVIE_ID")
+# if not prefix:
+#     print("MOVIE_ID is not set.")
+#     exit(1)
 
-with open(f"out/{prefix}_variant.m3u8", "r", encoding="utf-8") as input_file:
-    lines = input_file.readlines()
+def patch(prefix):
+    path = pathlib.Path(f"out/{prefix}/{prefix}_variant.m3u8")
+    if not path.exists():
+        return
 
-count = 1
-for i, line in enumerate(lines):
-    if line.startswith("http"):
-        lines[i] = f"{prefix}_segment_{count}.ts\n"
-        count += 1
+    with open(path, "r", encoding="utf-8") as input_file:
+        lines = input_file.readlines()
 
-with open(f"out/{prefix}.m3u8", "w", encoding="utf-8") as output_file:
-    output_file.writelines(lines)
+    count = 1
+    for i, line in enumerate(lines):
+        if line.startswith("http"):
+            lines[i] = f"{prefix}_segment_{count}.ts\n"
+            count += 1
+
+    path = pathlib.Path(f"out/{prefix}/{prefix}_variant.m3u8")
+    with open(path, "w", encoding="utf-8") as output_file:
+        output_file.writelines(lines)
 
 
-print(f"Patched playlist saved to out/{prefix}.m3u8")
-print(f"Total segments patched: {count - 1}")
+    print(f"Patched playlist saved to out/{prefix}.m3u8")
+    print(f"Total segments patched: {count - 1}")
